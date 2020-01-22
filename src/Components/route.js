230f651c5e663973bf26d001dd40e690d4e97187
Josh <joshcv2112@gmail.com>
2020-01-21 23:34:02 -0700

    reworked Route component

--- src/Components/route.js
+++ src/Components/route.js
@@ -2,20 +2,44 @@ import React, { Component } from 'react';
 
 class Route extends Component {
 
-    createTable = () => {
-        let table = []
+    state = {
+        loading: true,
+        routeList: null,
+    };
+    
+    async componentDidMount() {
+        const url = "https://developer.trimet.org/ws/V1/routeConfig/appID/5D6544C9A9CD1ED345B3CC22E/json/true";
+        const response = await fetch(url);
+        const data = await response.json();
+        const routeList = [];
+        data.resultSet.route.forEach(element => routeList.push(element));
+        //data.resultSet.route.forEach(element => routeDescList.push(element.desc));
+        this.setState({route: data.resultSet.route[0], routeList: routeList, loading: false});
+        console.log(this.state.routeList);
+    }
 
-        for (let j = 0; j < this.props.routeList.length; j++) {
-            table.push(<li key={j}>{this.props.routeList[j].desc}</li>)
+    createTable = () => {
+        let table  = [];
+        for (let i = 0; i < this.state.routeList.length; i++) {
+            table.push(<li key={i}>{this.state.routeList[i].desc}</li>);
         }
-        return table
-    }    
+        return table;
+    }
 
     render(){
         return(
-            <ul>
-                {this.createTable()}
-            </ul>
+            <div>
+                {this.state.loading || !this.state.route ? (
+                    <p>LOADING . . .</p>
+                ) : (
+                    <div>
+                        <h1>TriMet Routes</h1>
+                        <ul>
+                            {this.createTable()}
+                        </ul>
+                    </div>
+                )}
+            </div>
         );
     }
 }
