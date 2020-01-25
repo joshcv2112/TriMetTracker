0fe57c90a743c0220a8bca5e58f638e8848fc4a7
Josh <joshcv2112@gmail.com>
2020-01-24 22:01:48 -0700

    divided bus/rail lines

--- src/transit_routes.js
+++ src/transit_routes.js
@@ -14,30 +14,49 @@ class TransitRoutes extends Component {
         const routeList = [];
         data.resultSet.route.forEach(element => routeList.push(element));
         this.setState({route: data.resultSet.route[0], routeList: routeList, loading: false});
+        console.log(data);
     }
 
-    createTable = () => {
+    createMAXTable = () => {
         let table  = [];
         for (let i = 0; i < this.state.routeList.length; i++) {
-            table.push(<li key={i}>{this.state.routeList[i].desc}</li>);
+            if (this.state.routeList[i].type == "R")
+                table.push(<li key={i}>{this.state.routeList[i].desc}</li>);
+        }
+        return table;
+    }
+
+    createBusTable = () => {
+        let table  = [];
+        for (let i = 0; i < this.state.routeList.length; i++) {
+            if (this.state.routeList[i].type == "B")
+                table.push(<li key={i}>{this.state.routeList[i].desc}</li>);
         }
         return table;
     }
 
     render(){
         return(
-            <div>
-                {this.state.loading || !this.state.route ? (
-                    <p>LOADING . . .</p>
-                ) : (
-                    <div>
-                        <h1>TriMet Routes</h1>
-                        <ul>
-                            {this.createTable()}
-                        </ul>
-                    </div>
-                )}
-            </div>
+                <div>
+                    {this.state.loading || !this.state.route ? (
+                        <p>LOADING . . .</p>
+                    ) : (
+                        <div>
+                            <div>
+                                <h1>TriMet Rail Lines</h1>
+                                <ul>
+                                    {this.createMAXTable()}
+                                </ul>
+                            </div>
+                            <div>
+                                <h1>TriMet BUS Routes</h1>
+                                <ul>
+                                    {this.createBusTable()}
+                                </ul>
+                            </div>
+                        </div>
+                    )}
+                </div>
         );
     }
 }
